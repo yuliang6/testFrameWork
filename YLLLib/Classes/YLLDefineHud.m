7dfffa6e745d84bfdeed2b4a417479d5e68dda06
yuliangliang <yuliang1345@163.com>
2018-07-01 11:11:05 +0800

    第一次版本提交

---
+++ YLLLib/Classes/YLLDefineHud.m
@@ -0,0 +1,14 @@
+//
+//  YLLDefineHud.m
+//  SVProgressHUD
+//
+//  Created by 于亮亮 on 2018/7/1.
+//
+
+#import "YLLDefineHud.h"
+
+@implementation YLLDefineHud
+- (void)testLog{
+    NSLog(@"这里测试打印的东西");
+}
+@end
